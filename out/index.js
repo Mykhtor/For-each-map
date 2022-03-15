06a7c4c285130b6bbbbd54b7464cec866a087e44
polatovasadbek@gmail.com <rqalandarov0@gmail.com>
2022-03-15 12:05:47 +0500

    for each map

---
+++ out/index.js
@@ -0,0 +1,24 @@
+"use strict";
+let users = [
+    { name: 'Olimjon', age: 12 },
+    { name: 'Javohir', age: 13 },
+    { name: 'temur', age: 20 },
+    { name: 'Tozo bola', age: 14 },
+    { name: 'Oldosdf00', age: 17 },
+];
+// for (let i = 0; i <= users.length; i++) {
+//     const element = users[i]
+//     console.log(element);
+// }
+// for (const element of users ) {
+//     if ( element.age <= 13) {
+//         console.log(element.name);
+//     }
+// }
+let grownUsers = [];
+for (const element of users) {
+    if (element.age > 11 && element.age < 20) {
+        grownUsers.push(element);
+    }
+}
+console.log(grownUsers);
